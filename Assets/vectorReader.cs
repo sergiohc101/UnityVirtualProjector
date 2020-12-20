3e4b1feb761775de6c981f417c1a6c33e28ef086
Sergio Herrera <sergiohc101@gmail.com>
2020-12-20 18:30:09 +0100

    Code and scene cleanup

--- Assets/vectorReader.cs
+++ Assets/vectorReader.cs
@@ -1,46 +1,43 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class vectorReader : MonoBehaviour {
+public class vectorReader : MonoBehaviour
+{
+    public TextAsset textAsset;
 
-	public TextAsset textAsset;
+    // Initialization
+    void Start()
+    {
+        print("# Processing content: \n" + textAsset.text);
 
-	// Use this for initialization
-	void Start () {
+        string[] lines = textAsset.text.Split("\n"[0]); // gets all lines into separate strings
 
-		print(textAsset.text);
+        print("# NLines= " + lines.Length);
 
-		string[] lines = textAsset.text.Split("\n"[0]); // gets all lines into separate strings
+        // Print all lines one by one
+        // for (int i = 0; i < lines.Length; i++) {
+        // 	print( lines[i] );
+        // }
 
-		print(" NLines= " + lines.Length );
+        // Store the points into a Vector3 array
+        Vector3[] vectors = new Vector3[lines.Length];
+        for (var i = 0; i < lines.Length; i++)
+        {
+            var pt = lines[i].Split(","[0]); // gets 3 parts of the vector into separate strings
+            var x = float.Parse(pt[0]);
+            var y = float.Parse(pt[1]);
+            var z = 0.0f;   //float.Parse(pt[2]);
+            vectors[i] = new Vector3(x, y, z);
 
-		//for (int i = 0; i < lines.Length; i++) {
-		//	print( lines[i] );
-		//}
-		///*
-		Vector3[] vectors = new Vector3[ lines.Length ];
-			
+            print("# V[" + i + "]= " + vectors[i]);
+        }
 
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
-		vectors = new Vector3[lines.Length];
-		for (var i = 0; i < lines.Length; i++)
-		{
-			var pt = lines[i].Split(","[0]); // gets 3 parts of the vector into separate strings
-			var x = float.Parse(pt[0]);
-			var y = float.Parse(pt[1]);
-			var z = 0.0f; 	//float.Parse(pt[2]);
-			vectors[i] = new Vector3(x,y,z);
-
-			print( "V["+i+"]= " + vectors[i] );
-		}
-		//*/
-			
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
 }
